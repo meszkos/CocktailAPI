4dba27f22952eebe3b337d684df0f262e2001250
meszkos <michalik515@gmail.com>
2022-05-21 19:08:35 +0200

    Initial Commit

---
+++ CocktailAPI/CocktailAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailAPIApp.swift
+//  CocktailAPI
+//
+//  Created by MacOS on 21/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct CocktailAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
